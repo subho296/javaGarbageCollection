6f4da6aac3d72aa4bf96bd5dc1713bfb7cb32024
Subhodeep Dutta <subho296@gmail.com>
2018-10-10 08:45:41 +0530

    Added try catch in the test cases

--- src/test/java/org/subho/gc/test/GCTest.java
+++ src/test/java/org/subho/gc/test/GCTest.java
@@ -1,44 +1,63 @@
 package org.subho.gc.test;
 
+import org.junit.Assert;
 import org.subho.gc.GC;
 import org.subho.gc.test.entity.A;
 import org.subho.gc.test.entity.B;
 import org.subho.gc.test.entity.C;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GCTest {
+
+    public List<ArrayList<String>> map = new ArrayList<ArrayList<String>>();
 
     @Test
     public void testGCLifecycle(){
-        A a1 = new A(new B());
 
-        GC.get(a1);
-        GC.release(a1);
+        try {
+            A a1 = new A(new B());
 
-        GC.gc();
+            GC.get(a1);
+            GC.release(a1);
+
+            GC.gc();
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
 
     @Test
     public void testWithoutFinalize(){
-        C c1 = new C();
+        try {
+            C c1 = new C();
 
-        GC.get(c1);
-        GC.release(c1);
+            GC.get(c1);
+            GC.release(c1);
 
-        GC.gc();
+            GC.gc();
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
 
     @Test
     public void testWithCommonObject(){
-        B b1 = new B();
-        A a1 = new A(b1);
-        A a2 = new A(b1);
+        try {
+            B b1 = new B();
+            A a1 = new A(b1);
+            A a2 = new A(b1);
 
-        GC.get(a1);
-        GC.get(a2);
-        GC.release(a1);
+            GC.get(a1);
+            GC.get(a2);
+            GC.release(a1);
 
-        GC.gc();
+            GC.gc();
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
 
 
